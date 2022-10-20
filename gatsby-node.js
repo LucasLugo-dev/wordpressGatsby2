44489424ec412349f7c79c05f5012ef587d406ad
LucasLugo.dev <llugo@emfasi.com>
2022-10-20 12:26:22 +0200

    First commit second try Word-Gast

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,9 +1,31 @@
-exports.createPages = async ({ actions }) => {
-  const { createPage } = actions
-  createPage({
-    path: "/using-dsg",
-    component: require.resolve("./src/templates/using-dsg.js"),
-    context: {},
-    defer: true,
-  })
+exports.createPages = async ({ actions, graphql, reporter }) => {
+  const result = await graphql(`
+    {
+      allWpPost {
+        nodes {
+          id
+          uri
+        }
+      }
+    }
+  `)
+
+  if (result.errors) {
+    reporter.error("There was an error fetching posts", result.errors)
+  }
+
+  const { allWpPost } = result.data
+
+  // Define the template to use
+  const template = require.resolve(`./src/templates/WpPost.js`)
+
+  if (allWpPost.nodes.length) {
+    allWpPost.nodes.map(post => {
+      actions.createPage({
+        path: post.uri,
+        component: template,
+        context: post,
+      })
+    })
+  }
 }
